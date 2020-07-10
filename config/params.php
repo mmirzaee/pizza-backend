fbf89a7137f85c547c694c7957fb763b95b41c17
Ali M.Mirzaee <me@alimrz.com>
2020-07-10 19:31:03 +0430

    Increased JWT expiration time

--- config/params.php
+++ config/params.php
@@ -6,6 +6,6 @@ return [
     'TokenID' => 'Ssdfkm0c42c2r24crr2',
     'JwtIssuer' => 'ChangeThisToIssuer',
     'JwtAudience' => 'ChangeThisToAudience',
-    'JwtExpire' => 3600,
+    'JwtExpire' => 60 * 60 * 24 * 30,  // JWT is valid for 30 days
     'DefaultSignupRole' => 'member',
 ];
